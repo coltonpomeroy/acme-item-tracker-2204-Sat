43c0164b256d91b8ea5b7824ef24026e936c61aa
ericpkatz <ericpkatz@gmail.com>
2022-07-08 20:49:27 -0400

    commit

---
+++ db/index.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/the_acme_item_tracker_db');
+
+const { STRING, INTEGER } = Sequelize;
+
+const User = conn.define('user', {
+  name: {
+    type: STRING 
+  }
+});
+
+const Thing = conn.define('thing', {
+  name: {
+    type: STRING 
+  },
+  ranking: {
+    type: INTEGER,
+    defaultValue: 1
+  }
+});
+
+Thing.belongsTo(User);
+
+module.exports = {
+  conn,
+  User,
+  Thing
+};
---
+++ src/Users.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+
+const Users = ({ users, createUser, deleteUser, things })=> {
+  return (
+    <div>
+      <h1>Users</h1>
+      <button onClick={ createUser }>+</button>
+      <ul>
+        {
+          users.map( user => {
+            return (
+              <li key={ user.id }>
+                { user.name }
+                <button onClick={ ()=> deleteUser(user)}>x</button>
+                <ul>
+                {
+                  things.filter( thing => thing.userId === user.id)
+                    .map(thing => {
+                      return (
+                        <li key={ thing.id }>
+                          { thing.name } ({ thing.ranking })
+                        </li>
+                      );
+                    }) 
+                  
+                }
+                </ul>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+const mapStateToProps = (state)=> {
+  return {
+    users: state.users,
+    things: state.things
+  };
+}
+
+const mapDispatch = (dispatch)=> {
+  return {
+    deleteUser: async(user)=> {
+      await axios.delete(`/api/users/${user.id}`);
+      dispatch({ type: 'DELETE_USER', user});
+    },
+    createUser: async()=> {
+      const user = (await axios.post('/api/users', {name: Math.random()})).data;
+      dispatch({ type: 'CREATE_USER', user});
+    }
+  };
+}
+export default connect(mapStateToProps, mapDispatch)(Users);
